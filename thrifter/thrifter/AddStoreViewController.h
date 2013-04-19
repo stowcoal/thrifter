b9334c31130eb01b983da17b581d0a3d4dad2082
CURTIS STOCHL <cdstochl@SH308-31.local>
2013-04-19 09:45:45 -0500

    Adding hours of stores.  Added all the labels and textfields.  need to implement for read and write stores as well as add store and store detail view.

--- thrifter/thrifter/AddStoreViewController.h
+++ thrifter/thrifter/AddStoreViewController.h
@@ -12,5 +12,19 @@
 @property (weak, nonatomic) IBOutlet UITextField *TextViewStoreName;
 @property (weak, nonatomic) IBOutlet UITextField *TextViewStoreCity;
 - (IBAction)ButtonDoneClick:(UIBarButtonItem *)sender;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewMondayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewTuesdayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewWednesdayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewThursdayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewFridayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewSaturdayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewSundayOpen;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewMondayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewTuesdayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewWednesdayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewThursdayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewFridayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewSaturdayClose;
+@property (weak, nonatomic) IBOutlet UITextField *TextViewSundayClose;
 
 @end
