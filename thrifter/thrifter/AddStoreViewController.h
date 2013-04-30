9eb7522698b57592ec6b79e88bcb23ff1664582f
CURTIS STOCHL <cdstochl@SH308-25.local>
2013-04-30 11:09:17 -0500

    fixing a lot of things

--- thrifter/thrifter/AddStoreViewController.h
+++ thrifter/thrifter/AddStoreViewController.h
@@ -5,7 +5,7 @@
 //  Created by CURTIS STOCHL on 2/22/13.
 //  Copyright (c) 2013 CURTIS STOCHL. All rights reserved.
 //
-
+#import "HoursClosedOpenButton.h"
 #import <UIKit/UIKit.h>
 
 @interface AddStoreViewController : UITableViewController
@@ -26,41 +26,22 @@
 @property (weak, nonatomic) IBOutlet UITextField *TextViewFridayClose;
 @property (weak, nonatomic) IBOutlet UITextField *TextViewSaturdayClose;
 @property (weak, nonatomic) IBOutlet UITextField *TextViewSundayClose;
+- (IBAction)ButtonAMPMClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonMondayFirst;
-- (IBAction)ButtonMondayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonTuesdayFirst;
-- (IBAction)ButtonTuesdayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonWednesdayFirst;
-- (IBAction)ButtonWednesdayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonThursdayFirst;
-- (IBAction)ButtonThursdayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonFridayFirst;
-- (IBAction)ButtonFridayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonSaturdayFirst;
-- (IBAction)ButtonSaturdayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonSundayFirst;
-- (IBAction)ButtonSundayFirstClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonMondaySecond;
-- (IBAction)ButtonMondaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonTuesdaySecond;
-- (IBAction)ButtonTuesdaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonWednesdaySecond;
-- (IBAction)ButtonWednesdaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonThursdaySecond;
-- (IBAction)ButtonThursdaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonFridaySecond;
-- (IBAction)ButtonFridaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonSaturdaySecond;
-- (IBAction)ButtonSaturdaySecondClick:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonSundaySecond;
-- (IBAction)ButtonSundaySecondClick:(UIButton *)sender;
-- (IBAction)ButtonMondayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonTuesdsayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonWednesdayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonThursdayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonFridayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonSaturdayClosedClick:(UIButton *)sender;
-- (IBAction)ButtonSundayClosedClick:(UIButton *)sender;
+- (IBAction)ButtonClosedClick:(HoursClosedOpenButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonMondayClosed;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonTuesdayClosed;
 @property (weak, nonatomic) IBOutlet UIButton *ButtonWednesdayClosed;
---
+++ thrifter/thrifter/HoursClosedOpenButton.h
@@ -0,0 +1,16 @@
+//
+//  HoursClosedOpenButton.h
+//  thrifter
+//
+//  Created by CURTIS STOCHL on 4/30/13.
+//  Copyright (c) 2013 CURTIS STOCHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HoursClosedOpenButton : UIButton
+@property (nonatomic) IBOutlet UIButton* firstButton;
+@property (nonatomic) IBOutlet UIButton* secondButton;
+@property (nonatomic) IBOutlet UITextField* openTextField;
+@property (nonatomic) IBOutlet UITextField* closedTextField;
+@end
